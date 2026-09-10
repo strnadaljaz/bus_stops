a1feb774b0f9d7a6004ad62b2ac96a2d92b88232
strnadaljaz <strnadaljaz23@gmail.com>
2026-09-10 10:45:40 +0200

    implementiral station class in pridobivanje imena postaje

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+
 class Main {
     static int station_id;
     static int num_buses_per_line;
     static TimeFormat time_format;
 
-    public static void main(String args[]) {
+    static final String stops_file = "./gtfs/stops.txt";
+
+    public static void main(String args[]) throws IOException {
         if (args.length != 3) {
             System.out.println("Wrong number of arguments");
             return;
@@ -12,5 +16,8 @@ class Main {
         station_id = Integer.parseInt(args[0]);
         num_buses_per_line = Integer.parseInt(args[1]);
         time_format = args[2] == "absolute" ? TimeFormat.absolute : TimeFormat.relative;
+
+        Station station = new Station(station_id, stops_file);
+
     }
 }
---
+++ Station.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+
+public class Station {
+    int id;
+    String name;
+
+    Station(int station_id, String stops_file) throws IOException {
+        this.id = station_id;
+
+        // Source - https://stackoverflow.com/a/5868528
+        // Posted by Peter Lawrey, modified by community. See post 'Timeline' for change
+        // history
+        // Retrieved 2026-09-10, License - CC BY-SA 4.0
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(stops_file)))) {
+            String line = br.readLine(); // prvo vrstico izpustimo
+
+            while ((line = br.readLine()) != null) {
+                String columns[] = line.split(",");
+                int id = Integer.parseInt(columns[0]);
+
+                if (id == this.id) {
+                    this.name = columns[2];
+                    break;
+                }
+            }
+        }
+    }
+
+    String getName() {
+        return this.name;
+    }
+}
