1a4157b17d4df6875132092627f2101e4ff48b69
strnadaljaz <strnadaljaz23@gmail.com>
2026-09-10 15:53:26 +0200

    test za branje Station iz stops.txt

--- src/test/java/integration/StationNameTest.java
+++ src/test/java/integration/StationNameTest.java
@@ -1,2 +1,14 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StationNameTest {
+
+    @Test
+    void StationConstructorGetsCorrectNameFromId() {
+        int id = 2;
+        String name = "AL Masjid Al-nabawi (Clock Roundabout)";
+        Station station = new Station(id, "./gtfs/stops.txt");
+
+        assertEquals(name, station.name);
+    }
+}
